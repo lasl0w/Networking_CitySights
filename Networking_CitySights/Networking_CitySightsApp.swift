8a7733cb99ce51ea906f0c1f5a243a9d95a1f7ff
Tom Montgomery <montgomery3681@gmail.com>
2024-07-08 14:37:13 -0600

    Initial Commit

---
+++ Networking_CitySights/Networking_CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Networking_CitySightsApp.swift
+//  Networking_CitySights
+//
+//  Created by tom montgomery on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Networking_CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
